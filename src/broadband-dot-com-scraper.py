6e1c572134cf7bf945233ad4a1c40ac2f7baa548
Karolina Mikiciuk <karolina-o-mikiciuk@wp.pl>
2021-12-01 12:05:17 +0000

    {Feature} Add code to scrape the ratings.

--- src/broadband-dot-com-scraper.py
+++ src/broadband-dot-com-scraper.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests as rq
 import csv
-from utils import get_review_content, get_review_date, get_review_location, get_review_meta_data, get_review_ratings, save_into_csv
+from utils import get_review_content, get_review_date, get_review_location, get_review_meta_data
+from utils import  get_review_ratings, save_into_csv, clean_review_ratings
 
 dest_folder = "/home/mrfox/Desktop/bt_group_project/data"
 page_start = 1
-page_end = 2 #81
+page_end = 81
 
 for page_num in range(page_start,page_end):
 
@@ -13,7 +14,7 @@ for page_num in range(page_start,page_end):
     reviews_contents = get_review_content(reviews_meta_data)
     reviews_dates = get_review_date(reviews_meta_data)
     reviews_locations = get_review_location(reviews_meta_data)
-    #reviews_ratings = get_review_ratings(reviews_meta_data)
-    #reviews_ratings = group_corresponding_ratings(reviews_ratings)
+    reviews_ratings = get_review_ratings(reviews_meta_data)
+    reviews_ratings = clean_review_ratings(reviews_ratings)
 
-    save_into_csv(dest_folder,page_num,reviews_locations, reviews_dates, reviews_contents)
+    save_into_csv(dest_folder,page_num,reviews_locations, reviews_dates, reviews_contents, reviews_ratings)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
+import requests as rq
 import csv
-
+from bs4 import BeautifulSoup
 
 def get_review_meta_data(page_num):
     """Returns all metadata for reviews on specified page."""
@@ -10,7 +11,7 @@ def get_review_meta_data(page_num):
     content_soup = BeautifulSoup(result_fetched.content, 'lxml') #using lxml parser features='lxml'
 
     # find html of the review class in the content soup
-    reviews_all_meta = content_soup.find_all(class_="review") #class_="review" #class_="stars right"
+    reviews_all_meta = content_soup.find_all(class_="review") 
     return reviews_all_meta
 
 def get_review_content(reviews_all_meta):
@@ -47,25 +48,65 @@ def get_review_location(reviews_all_meta):
     return locations
 
 
-
-def save_into_csv(dest_folder,page_num, reviews_locations, reviews_dates, reviews_contents):
+def save_into_csv(dest_folder,page_num, reviews_locations, reviews_dates, reviews_contents, reviews_rating):
     """Saves data from page X to data_file_X.csv in dest_folder."""
     file_name = dest_folder+f"/data_file_{page_num}.csv"
-
+    
     with open(file_name,'a') as csv_file:
+        header = ['content','location','date','satisfaction','customer_service','speed','reliability,']
         writer = csv.writer(csv_file)
-        writer.writerow(['content','location','date','satisfaction','customer_service','speed','reliability'])
-
-        for i in range(len(reviews_dates)):
-
+        writer.writerow(header)
+                         
+        
+        for i in range(len(reviews_dates)): 
+                
             content = reviews_contents[i]
             location = reviews_locations[i]
             date = reviews_dates[i]
+            
+            satisfaction = reviews_rating[i][0] 
+            customer_service = reviews_rating[i][1]
+            speed = reviews_rating[i][2]
+            reliability = reviews_rating[i][3]
+            
+            data = [content,location,date, satisfaction,customer_service,speed,reliability]
+            writer.writerow(data)
 
-#             satisfaction = reviews_rating[i][0] # here list index out of range; 11 items in reviews, 40 in dates
-#             customer_service = reviews_rating[i][1]
-#             speed = reviews_rating[i][2]
-#             reliability = reviews_rating[i][3]
 
-            csv_file.write(f"{content},{location},{date},")
-           #",{satisfaction},{customer_service},{speed},{reliability},")
+def get_review_ratings(reviews_all_meta):
+    """Returns a list of where each element is all the ratings given. ~~~ for no ratings."""
+    ratings = []
+    for review in reviews_all_meta:
+        review_ratings = review.ul #.li   #('div',{"class" : "stars right"})
+
+        if review and review_ratings: 
+            rating = review_ratings.text.replace('\n','').replace('\t','').replace('\r','')
+            ratings.append(rating+"~~~") # Nones don't get displayed
+
+        else:
+            ratings.append("~~~")
+    return ratings
+
+def clean_review_ratings(ratings):
+    """Returns a list of lists with the ratings. Returns ~~~ for no rating given."""
+    rating_types = ['Satisfaction','Service','Speed','Reliability'] 
+    tuples = []
+    
+    for rating in ratings:
+        r_tuples = []
+        
+        rating_list = rating.split(' ')
+        rating_str =  "-".join(rating_list)
+        rating_str = rating_str.replace('Customer-','').replace('-stars','').replace('-star','')        
+        rating_list = rating_str.split('-')
+            
+        for rating_type in rating_types:
+            
+            if rating_type in rating_list:
+                index = rating_list.index(rating_type)
+                r_tuples.append(rating_list[index+1])    
+            elif rating_type not in rating_list:
+                r_tuples.append('~~~')
+        tuples.append(r_tuples)   
+        
+    return tuples
